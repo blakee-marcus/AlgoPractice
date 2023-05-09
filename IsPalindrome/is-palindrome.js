657e215020f2493ee8e4331c15bf2db2489a6277
bm <marcusb733@gmail.com>
2023-05-08 18:51:55 -0700

    Solve IsPalindrome

---
+++ IsPalindrome/is-palindrome.js
@@ -0,0 +1,10 @@
+// Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
+
+var isPalindrome = function(str) {
+    const reverseStr = str.trim().split('').reverse().join('');
+    if (str === reverseStr) {
+        return true;
+    } else {
+        return false;
+    }
+};
