1f8d7434820ad0cc43a5af6139ec5a1595d6a90a
bm <marcusb733@gmail.com>
2023-05-08 18:29:53 -0700

    Solve Max Num

---
+++ MaxNum/max-num.js
@@ -0,0 +1,11 @@
+// Write code to return the largest number in the given array
+
+var maxNum = function(arr) {
+    let max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+    }
+    return max;
+};
